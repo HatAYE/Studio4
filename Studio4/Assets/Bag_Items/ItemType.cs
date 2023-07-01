f78b75472db4f554e3722b1f14e262e4c6abc71d
HatAYE <h.alchaban@student.saedubai.com>
2023-07-01 23:30:04 +0400

    objects interactibility wip

--- Studio4/Assets/Bag_Items/ItemType.cs
+++ Studio4/Assets/Bag_Items/ItemType.cs
@@ -6,7 +6,10 @@ public class ItemType : MonoBehaviour
 {
     public int dangerValue;
     public int dangerPoints;
+    public bool illegalItem;
     /// <summary>
     /// The point value determines how dangerosu an item is from 0 to 3. 0 being comletely safe and 3 being high danger.
     /// </summary>
+
+    
 }
--- Studio4/Assets/PointSystem.cs
+++ Studio4/Assets/PointSystem.cs
@@ -6,24 +6,14 @@ using UnityEngine.UI;
 
 public class PointSystem : MonoBehaviour
 {
-    [SerializeField] int scorePoints;
+    int scorePoints;
     [SerializeField] BagReset bagReset;
     [SerializeField] BagMovement bagMovement;
-    [SerializeField] int itemPointValue;
     [SerializeField] TextMeshProUGUI scoreText;
 
-    void Start()
-    {
-
-    }
-    void GainPoints()
+    void CalculatePoints(int itemPointValue)
     {
         scorePoints += itemPointValue;
-    }
-
-    void LosePoints()
-    {
-        scorePoints -= 1 * itemPointValue;
     }
 
     public void ItemCheck()
@@ -34,38 +24,55 @@ public class PointSystem : MonoBehaviour
         {
             for (int i = 0; i < bagReset.objectRandomizer.Count; i++)
             {
-                Debug.Log(bagReset.objectRandomizer[i]);
                 int itemValue;
                 Transform child = bagReset.objectRandomizer[i].transform.GetChild(0);
 
                 if (child.gameObject.GetComponent<ItemType>() != null)
                 {
-                    itemValue = child.gameObject.GetComponent<ItemType>().dangerValue;
-                    itemPointValue = child.gameObject.GetComponent<ItemType>().dangerPoints;
-                    if (itemValue > 0)
-                    {
-                        Debug.Log("dangerous items were found");
-                        //LosePoints();
-                    }
-                    if (itemValue == 0)
-                    {
-                        Debug.Log("items were safe");
-                        //GainPoints();
-                    }
-                    GainPoints();
-                    Debug.Log("items value" + itemValue);
+                    itemValue = child.gameObject.GetComponent<ItemType>().dangerValue;          //DANGER VALUE determines how dangerous an item is from 0 to 3
+                    int itemPointValue = child.gameObject.GetComponent<ItemType>().dangerPoints;    //DANGER POINTS is the number calculated at the end of the baggage inspection
+                    CalculatePoints(itemPointValue);
                 }
             }
         }
 
     }
+    private void InteractingWithItem()
+    {
+        ///this function is supposed to detect whether the item clicked on is illegal or not. if player click on item and it is in fact illegal, item gets destroyed and players gains some points. if player clicks and item isn't illegal, he loses points and items remains.
+        Debug.Log("ONE");
 
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
-    // Update is called once per frame
+            if (hit.collider.gameObject.GetComponent<ItemType>() != null)
+            {
+                Debug.Log("TWO");
+
+                if (hit.collider.gameObject.GetComponent<ItemType>().illegalItem == true)
+                {
+                    Debug.Log("THREE");
+
+                    CalculatePoints(20);
+                    Destroy(hit.collider.gameObject);
+                }
+                else if (hit.collider.gameObject.GetComponent<ItemType>().illegalItem == false)
+                {
+                    Debug.Log("FOUR");
+
+                    CalculatePoints(-50);
+                }
+            }
+
+        
+    }
     void Update()
     {
-        Debug.Log("points:" + scorePoints);
         scoreText.text = scorePoints.ToString();
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            InteractingWithItem();
+        }
     }
 }
